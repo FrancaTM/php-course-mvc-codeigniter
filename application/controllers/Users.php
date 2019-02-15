b323ffba2c68410ee6a50a332baa3f744079362a
Túlio <tuliofranca@visionnaire.com.br>
2019-02-15 08:42:12 -0200

    Load model automatically Load database manual/automatically

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -5,8 +5,9 @@ class Users extends CI_Controller
     public function show()
     {
         // loads model manually
-        $this->load->model('user_model');
+        // $this->load->model('user_model');
 
         $result = $this->user_model->get_users();
+        
     }
 }
--- application/models/user_model.php
+++ application/models/user_model.php
@@ -4,6 +4,21 @@ class User_model extends CI_Model
 {
     public function get_users()
     {
+        $query = $this->db->get('users');
+        return $query->result();
 
+        // loads database automatically
+        // $config['hostname'] = 'localhost';
+        // $config['username'] = "root";
+        // $config['password'] = "";
+        // $config['database'] = "errand_db_course";
+
+        // $config_2['hostname'] = 'localhost';
+        // $config_2['username'] = "root";
+        // $config_2['password'] = "";
+        // $config_2['database'] = "errand_db_course_2";
+
+        // $connection = $this->load->database($config);
+        // $connection_2 = $this->load->database($config_2);
     }
 }
