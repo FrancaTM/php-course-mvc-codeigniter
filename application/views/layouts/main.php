0338da5a91fb5bce71d12b616414505d683ba7f6
Túlio <tuliofranca@visionnaire.com.br>
2019-02-15 20:16:39 -0200

    Starting ci form Configure autoload (form) Fix jquery reference name Configure login view (form)

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -7,7 +7,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
     <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
 </head>
 <body>
 
--- application/views/users/login_view.php
+++ application/views/users/login_view.php
@@ -1,1 +1,6 @@
 <h2>Login form</h2>
+
+<?php $attributes = array('id' => 'login_form', 'class' => 'form_horizontal'); ?>
+
+<?php echo form_open('users/login_view', $attributes); ?>
+<?php echo form_close(); ?>
