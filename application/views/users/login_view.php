faa46f5d0b0fdbd9b2efc9b1bb2d200d1f19d99a
Túlio <tuliofranca@visionnaire.com.br>
2019-02-15 20:28:17 -0200

    Create input and add attributes

--- application/views/users/login_view.php
+++ application/views/users/login_view.php
@@ -1,6 +1,20 @@
 <h2>Login form</h2>
 
-<?php $attributes = array('id' => 'login_form', 'class' => 'form_horizontal'); ?>
+<?php $attributes = array('id' => 'login_form', 'class' => 'form_horizontal');?>
 
 <?php echo form_open('users/login_view', $attributes); ?>
+
+<div class"form_group">
+<?php echo form_label('Username'); ?>
+
+<?php $data = array(
+    'class' => 'form-control',
+    'name' => 'username',
+    'placeholder' => 'Enter username'
+);
+?>
+
+<?php echo form_input($data); ?>
+</div>
+
 <?php echo form_close(); ?>
