ce002f8781a69b7c87eac6dcf8141fe7cca351f3
Túlio <tuliofranca@visionnaire.com.br>
2019-02-16 21:22:49 -0200

    Fix jquery file read permission Fix load order for jquery and bootstrap files

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -6,14 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+    <script src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
 </head>
 <body>
 
 <div class="container">
     <div class="col-xs-3">
-        <?php $this->load->view('users/login_view'); ?>
+        <?php $this->load->view('users/login_view');?>
     </div>
     <div class="col-xs-9">
         <?php $this->load->view($main_view);?>
